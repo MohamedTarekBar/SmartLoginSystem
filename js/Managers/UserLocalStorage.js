0d251d652b1bb21398521ab1f1cdaad37a10f80d
MohamedTarekBar <77550770+MohamedTarekBar@users.noreply.github.com>
2022-10-06 22:05:33 +0200

    Add files via upload

---
+++ js/Managers/UserLocalStorage.js
@@ -0,0 +1,67 @@
+'use strict';
+
+import { baseURL } from './path.js';
+import { constant } from './GlobalObjects.js';
+
+
+export default class UserLocalStorage {
+  constructor () {
+    this.isLoggedIn ();
+  }
+
+  isLoggedIn () {
+    if (localStorage.getItem (constant.isLoggedIn) == null) {
+      if (!window.location.pathname.includes ('index.html')) {
+        window.location = `${baseURL}index.html`;
+      }
+      return false;
+    } else {
+      if (localStorage.getItem (constant.isLoggedIn) == 'true') {
+        if (!window.location.pathname.includes ('profilepage.html')) {
+          window.location = `${baseURL}pages/profilepage.html`;
+        } else {
+        }
+      } else {
+        if (!window.location.pathname.includes ('index.html')) {
+          window.location = `${baseURL}index.html`;
+        }
+      }
+    }
+  }
+
+  setUserData (user) {
+    let stringify = JSON.stringify (user);
+    localStorage.setItem (constant.user, stringify);
+  }
+
+  getUserData () {
+    if (localStorage.getItem (constant.user) != null) {
+      let parse = JSON.parse (localStorage.getItem (constant.user));
+      return parse;
+    } else {
+        this.attackingGuard()
+    }
+  }
+
+  deleteUserData() {
+    localStorage.setItem(constant.user,null)
+  }
+
+  userLoggedIn () {
+    localStorage.setItem (constant.isLoggedIn, true);
+    this.isLoggedIn ();
+  }
+
+  userLoggedOut () {
+    localStorage.setItem (constant.isLoggedIn, false);
+    this.deleteUserData()
+    this.isLoggedIn ();
+  }
+
+  attackingGuard () {
+    localStorage.clear ();
+    if (!window.location.pathname.includes ('index.html')) {
+      window.location = `${baseURL}index.html`;
+    }
+  }
+}
---
+++ js/Managers/path.js
@@ -0,0 +1,2 @@
+
+export let  baseURL = "http://127.0.0.1:5500/"
